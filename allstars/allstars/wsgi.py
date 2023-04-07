3a2490cb3768e9168d324684cd7114b8f13fb0c6
cehceh <amr.jobmail@gmail.com>
2023-04-07 21:29:12 +0000

    Adjusting settings.py and wsgi.py files

--- allstars/allstars/wsgi.py
+++ allstars/allstars/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+
+# add your project directory to the sys.path
+project_home = '/home/amramerjob/all_stars/allstars/'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'allstars.settings')
 
