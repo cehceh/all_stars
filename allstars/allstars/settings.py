31246aea8798a31a225f247cdc778748993aadd0
cehceh <amr.jobmail@gmail.com>
2023-04-07 22:13:46 +0000

    Adjusting database config. in settings.py

--- allstars/allstars/settings.py
+++ allstars/allstars/settings.py
@@ -86,24 +86,22 @@ WSGI_APPLICATION = 'allstars.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db()
-    # 'default': {
-        
-
+    # "default": env.db()
+    'default': {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
 
-        # 'ENGINE':'django.db.backends.mysql',
-        # 'NAME': 'amramerjob$default',  # Name of the database itself
-        # 'USER':'amramerjob',
-        # 'PASSWORD': env['PASSWORD'],
-        # 'HOST': 'amramerjob.mysql.pythonanywhere-services.com',
-        # 'PORT':'3306',
-        # 'OPTIONS': {
-        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        # },
+        'ENGINE':'django.db.backends.mysql',
+        'NAME': 'amramerjob$default',  # Name of the database itself
+        'USER':'amramerjob',
+        'PASSWORD': env['PASSWORD'],
+        'HOST': 'amramerjob.mysql.pythonanywhere-services.com',
+        'PORT':'3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
 
-    # }
+    }
 }
 
 
